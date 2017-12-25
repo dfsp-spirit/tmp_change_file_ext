d753e8e49b8d5af3e563cb148bd562431da800d7
Tim Schäfer <ts+code@rcmd.org>
2017-12-25 02:46:29 +0100

    Update help function.

--- tmp_change_file_ext.sh
+++ tmp_change_file_ext.sh
@@ -8,28 +8,34 @@ APPTAG="[TMPEXTCH]"
 SEARCH_DIR="."
 ORIGINAL_EXTENSION="jsx"
 TEMP_EXTENSION="js"
-MODIFIED_FILES_LIST="~/.renamed_files.lst"
+MODIFIED_FILES_LIST="$HOME/.renamed_files.lst"
 
 ### There should be no need to modify stuff below this line.
 
 ### Functions
 
 function show_help {
-    echo "$APPTAG USAGE: $0 <options> [--force]"
+    echo "$APPTAG USAGE: $0 -c <options> [-f]"
+    echo "$APPTAG USAGE: $0 -r <options>"
+    echo "$APPTAG USAGE: $0 -h"
 	echo "$APPTAG valid options are:"
-	echo "$APPTAG   -c             :  set action mode to rename files according to settings"
-	echo "$APPTAG   -r              : set action mode to restore previsously renamed file to their original file extensions"
-	echo "$APPTAG   -s <ext>   : set source file extension to <ext>. Defaults to 'jsx' if omitted."
-	echo "$APPTAG   -t <ext>    : set temporary file extension to <ext>. Defaults to 'js' if omitted."
-	echo "$APPTAG   -d <dir>    : set directory to work on to <dir>. Defaults to '.' if omitted."
-	echo "$APPTAG   -f                : force action, possibly overwriting existing file extension mapping file."
-	echo "$APPTAG   -m <file>    : set mapping file location to <file>. Defaults to '~/.renamed_files.lst' if omitted."
-	echo "$APPTAG   -h                : show this help and exit"
+	echo "$APPTAG   -c         : change mode (rename files according to settings and create mapping file)"
+	echo "$APPTAG   -r         : restore mode (restore previsously renamed files to their original file extensions)"
+	echo "$APPTAG   -s <ext>   : set source or original file extension to <ext>. Defaults to 'jsx' if omitted."
+	echo "$APPTAG   -t <ext>   : set temporary file extension to <ext>. Defaults to 'js' if omitted."
+	echo "$APPTAG   -d <dir>   : set directory to work on to <dir>. Defaults to '.' if omitted."
+	echo "$APPTAG   -f         : force action, even if this means overwriting an existing file extension mapping file (only applies to change mode)."
+	echo "$APPTAG   -m <file>  : set mapping file location to <file>. Defaults to '~/.renamed_files.lst' if omitted."
+	echo "$APPTAG   -h         : show this help and exit"
 	echo "$APPTAG EXAMPLES:"
-	echo "$APPTAG * Rename all jsx files to js in ~/develop/my_project/:"
-	echo "$APPTAG    $0 -s jsx -t js -d ~/develop/my_project/ -c"
-	echo "$APPTAG * Restore the file names from the last action:"
-	echo "$APPTAG    $0 -s jsx -t js -d ~/develop/my_project/ -r"
+	echo "$APPTAG * Rename all jsx files to js in ~/my_project/:"
+	echo "$APPTAG    $0 -c -d ~/my_project/"
+	echo "$APPTAG   Restore the file names from the last action:"
+	echo "$APPTAG    $0 -r -d ~/develop/my_project/"
+    echo "$APPTAG * Rename all txt files to lst in /tmp/doc/:"
+	echo "$APPTAG    $0 -c -s txt -t lst -d /tmp/doc/"
+	echo "$APPTAG   Restore the file names from the last action:"
+	echo "$APPTAG    $0 -r -s txt -t lst -d /tmp/doc/"
 } 
 
 
@@ -69,14 +75,14 @@ shift $((OPTIND-1))
 
 if [ "$MODE" = "MODE_RENAME" ]; then
     if [ -f "$MODIFIED_FILES_LIST" -a ! "$FORCE" = "TRUE" ]; then
-        echo "$APPTAG WARNING: File list already exists at '$MODIFIED_FILES_LIST'. Re-running this script will erase the old file list. Add --force parameter to force this."
+        echo "$APPTAG WARNING: Mapping file already exists at '$MODIFIED_FILES_LIST'. Re-running this script would erase the old file list. Add -f parameter to force this action."
         exit 1
     fi
     if [ ! -f "$MODIFIED_FILES_LIST" ]; then
         touch "$MODIFIED_FILES_LIST"
     fi
     if [ ! -w "$MODIFIED_FILES_LIST" ]; then
-        echo "$APPTAG File list nto writable."
+        echo "$APPTAG Mapping file or directory at '$MODIFIED_FILES_LIST' not writable."
         exit 1
     fi
     find "${SEARCH_DIR}" -type f -name *.${ORIGINAL_EXTENSION} > ${MODIFIED_FILES_LIST}
