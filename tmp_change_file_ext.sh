ebcb6a207bc7a960de8cd9ade6a23c3107523dce
Tim Schäfer <ts+code@rcmd.org>
2017-12-25 02:28:42 +0100

    Use bash instead of sh

--- tmp_change_file_ext.sh
+++ tmp_change_file_ext.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # tmpextch -- Temporarily change the file extensions of files. The original file names can later be restored.
 # written by TS
 
@@ -17,21 +17,21 @@ MODIFIED_FILES_LIST="~/.renamed_files.lst"
 function show_help {
     echo "$APPTAG USAGE: $0 <options> [--force]"
 	echo "$APPTAG valid options are:"
-	echo "$APPTAG   -c    :  set action mode to rename files according to settings"
-	echo "$APPTAG   -r    : set action mode to restore previsously renamed file to their original file extensions"
-	echo "$APPTAG  -s <ext>               : set source file extension to <ext>. Defaults to 'jsx' if omitted."
-	echo "$APPTAG  -t <ext>               : set temporary file extension to <ext>. Defaults to 'js' if omitted."
-	echo "$APPTAG  -d <dir>               : set directory to work on to <dir>. Defaults to '.' if omitted."
-	echo "$APPTAG  -f                : force action, possibly overwriting existing file extension mapping file."
-	echo "$APPTAG -m <file>    : set mapping file location to <file>. Defaults to '~/.renamed_files.lst' if omitted."
-	echo "$APPTAG  -h                : show this help and exit"
+	echo "$APPTAG   -c             :  set action mode to rename files according to settings"
+	echo "$APPTAG   -r              : set action mode to restore previsously renamed file to their original file extensions"
+	echo "$APPTAG   -s <ext>   : set source file extension to <ext>. Defaults to 'jsx' if omitted."
+	echo "$APPTAG   -t <ext>    : set temporary file extension to <ext>. Defaults to 'js' if omitted."
+	echo "$APPTAG   -d <dir>    : set directory to work on to <dir>. Defaults to '.' if omitted."
+	echo "$APPTAG   -f                : force action, possibly overwriting existing file extension mapping file."
+	echo "$APPTAG   -m <file>    : set mapping file location to <file>. Defaults to '~/.renamed_files.lst' if omitted."
+	echo "$APPTAG   -h                : show this help and exit"
 	echo "$APPTAG EXAMPLES:"
 	echo "$APPTAG * Rename all jsx files to js in ~/develop/my_project/:"
 	echo "$APPTAG    $0 -s jsx -t js -d ~/develop/my_project/ -c"
 	echo "$APPTAG * Restore the file names from the last action:"
 	echo "$APPTAG    $0 -s jsx -t js -d ~/develop/my_project/ -r"
 } 
-				
+
 
 # Parse command line args (using getops)
 
